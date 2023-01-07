3fd058ac66ec48eb7fb8d95ff80a347e06c9897b
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-07 16:18:20 +0900

    두번째 섹션 추가 (#5)

--- src/views/index/Index.tsx
+++ src/views/index/Index.tsx
@@ -4,12 +4,14 @@ import { ScrollContextProvider } from '@/contexts/index.js';
 
 import LocalNav from './components/LocalNav.js';
 import ScrollSection1 from './ScrollSection1.js';
+import ScrollSection2 from './ScrollSection2.js';
 
 export default function Index() {
   return (
     <ScrollContextProvider>
       <LocalNav />
       <ScrollSection1 />
+      <ScrollSection2 />
     </ScrollContextProvider>
   );
 }
